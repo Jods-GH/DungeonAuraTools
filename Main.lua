a3b2a27c94d13105ffa243bc71f956e67f75a894
Jods <39654549+Jodsderechte@users.noreply.github.com>
2022-09-01 12:40:56 +0200

    changed options generating to not error on first load

--- Main.lua
+++ Main.lua
@@ -4,11 +4,12 @@ local AceConfig = LibStub("AceConfig-3.0")
 local AceConfigDialog = LibStub("AceConfigDialog-3.0")
 local AceDBOptions = LibStub("AceDBOptions-3.0")
 ---@class MyAddon : AceAddon-3.0, AceConsole-3.0, AceConfig-3.0, AceGUI-3.0, AceConfigDialog-3.0
-local DungeonAuraTools = LibStub("AceAddon-3.0"):NewAddon("DungeonAuraTools", "AceConsole-3.0")
+local DungeonAuraTools = LibStub("AceAddon-3.0"):NewAddon("DungeonAuraTools", "AceConsole-3.0", "AceEvent-3.0")
 
 function DungeonAuraTools:OnInitialize()
 	-- Called when the addon is loaded
 	self:Print("Access the options via /jdt")
+    self:RegisterEvent("PLAYER_ENTERING_WORLD")
 
     local DatatoPersist = {
         profile = JDT.SpellList
@@ -25,8 +26,7 @@ function DungeonAuraTools:OnInitialize()
     self:RegisterChatCommand("jdt", "SlashCommand")
 	self:RegisterChatCommand("Jdt", "SlashCommand")
     self:RegisterChatCommand("DungeonAuraTools", "SlashCommand")
-
-    JDT.createOptionsData()   -- generates Data for Options Table
+  -- generates Data for Options Table
 
     function JDT.CreateOptionsFrame () 
         AceConfigDialog:Open(appName)
@@ -44,4 +44,10 @@ end
 
 function DungeonAuraTools:SlashCommand(msg) -- called when slash command is used
     JDT.CreateOptionsFrame() 
+end
+
+function DungeonAuraTools:PLAYER_ENTERING_WORLD(event, isLogin, isReload)
+    if isLogin then
+    JDT.createOptionsData() 
+    end
 end
