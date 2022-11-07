19364a9d9d3347527d35113890374b61bd36ece6
Jods <39654549+Jodsderechte@users.noreply.github.com>
2022-11-07 12:48:47 +0100

    Ui Improvements
    added expansion und dungeon specifc toggles fixes#48
    
    is also an improvement to #29

--- SpellDataValues/ExpansionValues.lua
+++ SpellDataValues/ExpansionValues.lua
@@ -1,13 +1,13 @@
 local _, JDT = ...
 
 JDT.ExpansionValues = JDT.ExpansionValues or {}
-JDT.ExpansionValues.Dragonflight = "https://wago.io/DF-Dungeons"
-JDT.ExpansionValues.Shadowlands = "https://wago.io/SL-Dungeons"
-JDT.ExpansionValues.BattleForAzeroth = "https://wago.io/BFA-Dungeons"
-JDT.ExpansionValues.Legion = "https://wago.io/Legion-Dungeons"
-JDT.ExpansionValues.WarlordOfDraenor = "https://wago.io/WOD-Dungeons"
-JDT.ExpansionValues.MistOfPandaria = "https://wago.io/MOP-Dungeons"
-JDT.ExpansionValues.Affixes = " "
+JDT.ExpansionValues.Dragonflight = { [1] = "https://wago.io/DF-Dungeons", [2] = LE_EXPANSION_DRAGONFLIGHT}
+JDT.ExpansionValues.Shadowlands = { [1] = "https://wago.io/SL-Dungeons", [2] = 	LE_EXPANSION_SHADOWLANDS}
+JDT.ExpansionValues.BattleForAzeroth = { [1] = "https://wago.io/BFA-Dungeons", [2] = LE_EXPANSION_BATTLE_FOR_AZEROTH}
+JDT.ExpansionValues.Legion = { [1] = "https://wago.io/Legion-Dungeons", [2] = LE_EXPANSION_LEGION}
+JDT.ExpansionValues.WarlordOfDraenor = { [1] = "https://wago.io/WOD-Dungeons", [2] = 	LE_EXPANSION_WARLORDS_OF_DRAENOR}
+JDT.ExpansionValues.MistOfPandaria = { [1] = "https://wago.io/MOP-Dungeons", [2] = LE_EXPANSION_MISTS_OF_PANDARIA}
+JDT.ExpansionValues.Affixes = { [1] = " "}
 
 setmetatable(JDT.ExpansionValues, {
     __index = function(_, key)
