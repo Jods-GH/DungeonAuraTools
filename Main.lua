1d702abe6f93bffce95d2b5eea7105ee8ad41850
Jods <39654549+Jodsderechte@users.noreply.github.com>
2023-06-21 22:35:43 +0200

    internal version bump for wa 5.5.5

--- Main.lua
+++ Main.lua
@@ -6,7 +6,7 @@ local SharedMedia = LibStub("LibSharedMedia-3.0")
 ---@class MyAddon : AceAddon-3.0, AceConsole-3.0, AceConfig-3.0, AceGUI-3.0, AceConfigDialog-3.0
 local DungeonAuraTools = LibStub("AceAddon-3.0"):NewAddon("DungeonAuraTools", "AceConsole-3.0", "AceEvent-3.0")
 JDT.AddonVersion = C_AddOns.GetAddOnMetadata(appName, "Version")
-JDT.InternalWaVersion = 65 -- version of weakauras addon on creation might need to be updated eventually
+JDT.InternalWaVersion = 66 -- version of weakauras addon on creation might need to be updated eventually
 
 JDT.FontMedias = JDT.FontMedias or {} 
 for _,v in pairs(SharedMedia:List(SharedMedia.MediaType.FONT)) do
