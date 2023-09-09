50ff62dfa0f71a237883148d720b323ee0176275
Jods <39654549+Jodsderechte@users.noreply.github.com>
2023-09-09 12:08:15 +0200

    add remaining BlackRookHold bosses and trash

--- Templates/Glows.lua
+++ Templates/Glows.lua
@@ -46,3 +46,26 @@ JDT.Templates.Glows.Ants = 	{
 					glowBorder = false,
 					glowDuration=1, 
 				} 			
+
+
+JDT.Templates.Glows.Pixel ={
+					glowFrequency = 0.65,
+					type = "subglow",
+					glowDuration = 1,
+					glowType = "Pixel",
+					glowLength = 10,
+					glowYOffset = 0,
+					glowColor = {
+						1, -- [1]
+						1, -- [2]
+						1, -- [3]
+						1, -- [4]
+					},
+					useGlowColor = true,
+					glowXOffset = 0,
+					glow = true,
+					glowThickness = 1,
+					glowScale = 1,
+					glowLines = 8,
+					glowBorder = false,
+				}
