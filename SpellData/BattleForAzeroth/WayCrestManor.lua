8e65e913e418c6e25ca9a5d3ffed39d46c75bb12
Florian Reichle <39654549+Jodsderechte@users.noreply.github.com>
2023-12-07 21:28:25 +0100

    add WM DinnerBell

--- SpellData/BattleForAzeroth/WayCrestManor.lua
+++ SpellData/BattleForAzeroth/WayCrestManor.lua
@@ -475,5 +475,17 @@ bosses.Trash = {
                 }
             },
         },
+        [JDT.GroupTypes.RunOut] ={
+            DinnerBell = {
+                enabled = true,
+                spellId = 265407, -- for spellname etc
+                triggerData = {
+                    {
+                        unit = JDT.Templates.Triggers.UnitTypes.nameplate,
+                        spellId = "265407",
+                    },
+                }
+            },
+        },
     }
 }
