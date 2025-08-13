899dede64d73aae8207548dfd88a6d93adcb0a1d
Jodsderechte <39654549+Jodsderechte@users.noreply.github.com>
2025-08-13 10:53:44 +0200

    add Eco Dome Trash

--- SpellData/TheWarWithin/EcoDomeAldani.lua
+++ SpellData/TheWarWithin/EcoDomeAldani.lua
@@ -269,6 +269,273 @@ bosses.boss3 = { --	Soul-Scribe
 bosses.Trash = { 
     additionalName = "TRASH] ",
     Auras = {
-        
+        [JDT.GroupTypes.InterruptableTargetedCast] ={
+            Gorge = {
+                enabled = true,
+                spellId = 1229474, -- for spellname etc
+                triggerData = {
+                     {
+                        unit = JDT.Templates.Triggers.UnitTypes.nameplate,
+                        spellId = "1229474",
+                    },
+                }
+            },
+            ArcaneBolt = {
+                enabled = true,
+                spellId = 1222815, -- for spellname etc
+                triggerData = {
+                     {
+                        unit = JDT.Templates.Triggers.UnitTypes.nameplate,
+                        spellId = "1222815",
+                    },
+                }
+            },
+        },
+        [JDT.GroupTypes.RunOut] ={
+            GorgingSmash = {
+                enabled = true,
+                spellId = 1221152, -- for spellname etc
+                triggerData = {
+                    {
+                        unit = JDT.Templates.Triggers.UnitTypes.nameplate,
+                        spellId = "1221152",
+
+                    },
+
+                }
+            },
+        },
+        [JDT.GroupTypes.CastIntoPlayerGroupDebuffSpread ] ={
+            GluttonousMiasma= {
+                enabled = true,
+                spellId = 1221190, -- for spellname etc
+                triggerData = {
+                    {
+                        unit = JDT.Templates.Triggers.UnitTypes.boss,
+                        spellId = "1221190",
+                    },
+                    {
+                        unit = JDT.Templates.Triggers.UnitTypes.player,
+                        spellId = "1221190",
+                    },
+                    {
+                        unit = JDT.Templates.Triggers.UnitTypes.smartGroup,
+                        spellId = "1221190",
+                    },
+                }
+            },
+            ArcingEnergy= {
+                enabled = true,
+                spellId = 1221483, -- for spellname etc
+                type = JDT.AuraTypes.magic,
+                triggerData = {
+                    {
+                        unit = JDT.Templates.Triggers.UnitTypes.nameplate,
+                        spellId = "1221483",
+                    },
+                    {
+                        unit = JDT.Templates.Triggers.UnitTypes.player,
+                        spellId = "1221483",
+                    },
+                    {
+                        unit = JDT.Templates.Triggers.UnitTypes.smartGroup,
+                        spellId = "1221483",
+                    },
+                }
+            }
+        },
+        [JDT.GroupTypes.TargetedCastWithTargetAnounce] ={
+            VolatileEjection = {
+                enabled = true,
+                spellId = 1226111, -- for spellname etc
+                triggerData = {
+                    {
+                        unit = JDT.Templates.Triggers.UnitTypes.nameplate,
+                        spellId = "1226111",
+
+                    },
+
+                }
+            },
+            ArcingZap = {
+                enabled = true,
+                spellId = 1229510, -- for spellname etc
+                type = JDT.AuraTypes.interrupt,
+                triggerData = {
+                    {
+                        unit = JDT.Templates.Triggers.UnitTypes.nameplate,
+                        spellId = "1229510",
+
+                    },
+
+                }
+            },
+            BurrowCharge = {
+                enabled = true,
+                spellId = 1237195, -- for spellname etc
+                triggerData = {
+                    {
+                        unit = JDT.Templates.Triggers.UnitTypes.nameplate,
+                        spellId = "1237195",
+
+                    },
+
+                }
+            },
+        },
+        [JDT.GroupTypes.FrontalIntoDot] = {
+            ArcaneSlash = {
+                enabled = true,
+                spellId = 1235368,
+                triggerData = {
+                    {
+                        unit = JDT.Templates.Triggers.UnitTypes.nameplate,
+                        spellId = "1235368",
+                    },
+                    {
+                        unit = JDT.Templates.Triggers.UnitTypes.player,
+                        spellId = "1235368",
+                    },
+
+                }
+            },
+        },
+        [JDT.GroupTypes.CastIntoAoeDebuffWithNextTick] ={
+            UnstableCore = {
+                enabled = true,
+                spellId = 1231244, -- for spellname etc
+                triggerData = {
+                    {
+                        unit = JDT.Templates.Triggers.UnitTypes.nameplate,
+                        spellId = "1231244",
+
+                    },
+                    {
+                        duration = "1.5",
+                        spellId = "1231252",
+
+                    },
+                    {
+                        unit = JDT.Templates.Triggers.UnitTypes.nameplate,
+                        spellId = "1231244",
+
+                    },
+
+                }
+            },
+            ErraticRitual = {
+                enabled = true,
+                spellId = 1221532, -- for spellname etc
+                triggerData = {
+                    {
+                        unit = JDT.Templates.Triggers.UnitTypes.nameplate,
+                        spellId = "1221532",
+
+                    },
+                    {
+                        duration = "0.5",
+                        spellId = "1221533",
+
+                    },
+                    {
+                        unit = JDT.Templates.Triggers.UnitTypes.nameplate,
+                        spellId = "1221532",
+
+                    },
+
+                }
+            }
+        },
+        [JDT.GroupTypes.Frontal] = {
+            Warp = {
+                enabled = true,
+                spellId = 1222356,
+                triggerData = {
+                    {
+                        unit = JDT.Templates.Triggers.UnitTypes.nameplate,
+                        spellId = "1222356",
+                    }
+
+                }
+            },
+        },
+        [JDT.GroupTypes.CastIntoKillAddAlive] = {
+            ConsumeSpirit = {
+                enabled = true,
+                spellId = 1226306,
+                triggerData = {
+                    {
+                        unit = JDT.Templates.Triggers.UnitTypes.nameplate,
+                        spellId = "1226306",
+                    },
+                    {
+                        unit = JDT.Templates.Triggers.UnitTypes.nameplate,
+                        npcID = "240952",
+                    },
+                }
+            },
+        },
+        [JDT.GroupTypes.AbsorbDebuffWithProgress] ={
+            GloomBite = {
+                enabled = true,
+                spellId = 1222341, -- for spellname etc
+                customTextInfo = { tooltip = 1, inverse = true},
+                triggerData = {
+                    {
+                        unit = JDT.Templates.Triggers.UnitTypes.player,
+                        spellId = "1222341",
+                    },
+                }
+            },
+        },
+        [JDT.GroupTypes.TargetedCast] = {
+            GloomBite = {
+                enabled = true,
+                spellId = 1222341,
+                extraName = " cast",
+                triggerData = {
+                    {
+                        unit = JDT.Templates.Triggers.UnitTypes.nameplate,
+                        spellId = "1222341",
+                    }
+                }
+            },
+        },
+         [JDT.GroupTypes.VoidCast] = {
+            BurrowingEruption = {
+                enabled = true,
+                spellId = 1223007,
+                triggerData = {
+                    {
+                        unit = JDT.Templates.Triggers.UnitTypes.nameplate,
+                        spellId = "1223007",
+                    },
+
+                }
+            },
+            Earthcrusher = {
+                enabled = true,
+                spellId = 1215850,
+                triggerData = {
+                    {
+                        unit = JDT.Templates.Triggers.UnitTypes.nameplate,
+                        spellId = "1215850",
+                    },
+
+                }
+            },
+        },
+        [JDT.GroupTypes.Dot] ={
+            StingingSandstorm  = {
+                enabled = true,
+                spellId = 1237220, -- for spellname etc
+                triggerData = {
+                    {
+                        unit = JDT.Templates.Triggers.UnitTypes.player,
+                        spellId = "1237220",
+                    },
+                }
+            }
+        },
     },
 }
